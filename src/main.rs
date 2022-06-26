db971cc91973f356a6cc7d3d4cd26adda7fdf732
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-06-26 23:32:18 +0300

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Append to the given FILEs, do not overwrite
+    #[clap(short, long)]
+    append: bool,
+
+    /// Name of the FILEs to write to
+    #[clap(value_parser, value_name = "FILE")]
+    files: Vec<String>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+}
