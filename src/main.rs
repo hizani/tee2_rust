cc31ae5cc333db5b6204cd3af2d0aa818c5ff4f5
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-06-27 02:12:05 +0300

    Implement tee functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::io::{Read, Write};
+use std::{fs::OpenOptions, io};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -12,6 +14,32 @@ struct Cli {
     files: Vec<String>,
 }
 
+fn tee(append_flag: bool, filename: &str, input: &str) -> io::Result<()> {
+    let mut file = OpenOptions::new()
+        .create(true) // open or create
+        .write(true) // with write mode
+        .append(append_flag) // append if append flag is true
+        .truncate(!append_flag) // truncate if append flag is false
+        .open(filename)?;
+
+    write!(file, "{}", input)?;
+
+    Ok(())
+}
+
 fn main() {
     let cli = Cli::parse();
+
+    // read standart input to the buffer
+    let mut stdin = io::stdin();
+    let mut buf = String::new();
+    stdin.read_to_string(&mut buf).unwrap();
+
+    for filename in &cli.files {
+        if let Err(e) = tee(cli.append, filename, &buf) {
+            eprintln!("{}: {}", filename, e);
+        }
+    }
+
+    print!("{}", buf);
 }
